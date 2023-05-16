11976be0ce3d583b4fdcdd33e3ad66f5626f6eb9
sajjad ghorbani <sajjadr2001@gmail.com>
2023-05-16 20:54:59 +0330

    Start: impelement game service

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GlobalModule } from './modules/global/global.module';
+import { GameModule } from './modules/game/game.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { GlobalModule } from './modules/global/global.module';
       isGlobal: true,
     }),
     GlobalModule,
+    GameModule,
   ],
 })
 export class AppModule {}
