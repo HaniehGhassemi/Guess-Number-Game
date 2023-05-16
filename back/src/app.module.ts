6b8e2b7f44dba1d05abe7e16b5d25823f9dfab37
sajjad ghorbani <sajjadr2001@gmail.com>
2023-05-16 12:18:32 +0330

    Add: prisma service

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GlobalModule } from './modules/global/global.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    GlobalModule,
   ],
 })
 export class AppModule {}
---
+++ back/src/modules/global/services/prisma/prisma.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService {
+  prisma: PrismaClient;
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+}
