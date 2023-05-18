8d8046cf9d8268193bc19a5426747e9a1921f696
sajjad ghorbani <sajjadr2001@gmail.com>
2023-05-19 00:24:03 +0330

    Add: global custom error filter

---
+++ back/src/common/utils/all-exception-filter.ts
@@ -0,0 +1,33 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { ResponseDto } from '../types/response.dto';
+import { Response } from 'express';
+import { CommonError } from '../types/common-error.enum';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody: ResponseDto<string> = {
+      success: false,
+      data:
+        exception instanceof HttpException
+          ? exception.message
+          : CommonError.INTERNAL_SERVER_ERROR,
+    };
+
+    response.status(httpStatus).json(responseBody);
+  }
+}
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/utils/all-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3000);
 }
 bootstrap();
