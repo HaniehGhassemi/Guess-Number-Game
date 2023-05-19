3c21206a4647095a42252169ecf12f3bb73403e7
sajjad ghorbani <sajjadr2001@gmail.com>
2023-05-19 13:49:34 +0330

    Add: signup user validation error messages

--- back/src/modules/users/dto/sign-up.dto.ts
+++ back/src/modules/users/dto/sign-up.dto.ts
@@ -2,23 +2,41 @@ import {
   IsNotEmpty,
   IsEmail,
   IsStrongPassword,
-  Matches,
+  IsString,
+  MinLength,
+  MaxLength,
 } from 'class-validator';
 import { Compare } from 'src/modules/global/decorators/compare.decorator';
+import { UserServiceValidationError } from '../types/user-service-validation-errors.enum';
 export class SignUpDto {
-  @IsNotEmpty()
-  @Matches('^[A-Za-z][A-Za-z0-9_]{3,20}$')
+  @IsNotEmpty({ message: UserServiceValidationError.USERNAME_IS_EMPTY })
+  @IsString({ message: UserServiceValidationError.USERNAME_ISNOT_STRING })
+  @MinLength(3, { message: UserServiceValidationError.USERNAME_TOO_SHORT })
+  @MaxLength(20, { message: UserServiceValidationError.USERNAME_TOO_LONG })
   username: string;
-  @IsNotEmpty()
-  @IsEmail()
+  @IsNotEmpty({ message: UserServiceValidationError.EMAIL_IS_EMPTY })
+  @IsEmail({}, { message: UserServiceValidationError.EMAIL_IS_INVALID })
   email: string;
-  @IsNotEmpty()
+  @IsNotEmpty({ message: UserServiceValidationError.FULLNAME_IS_EMPTY })
+  @IsString({ message: UserServiceValidationError.FULLNAME_ISNOT_STRING })
   fullname: string;
-  @IsNotEmpty()
-  @IsStrongPassword()
+  @IsNotEmpty({ message: UserServiceValidationError.PASSWORD_IS_EMPTY })
+  @IsStrongPassword(
+    {},
+    {
+      message: UserServiceValidationError.PASSWORD_IS_WEAK,
+    },
+  )
   password: string;
-  @IsNotEmpty()
-  @IsStrongPassword()
-  @Compare('password')
+  @IsNotEmpty({ message: UserServiceValidationError.CONFIRM_PASSWORD_IS_EMPTY })
+  @IsStrongPassword(
+    {},
+    {
+      message: UserServiceValidationError.CONFIRM_PASSWORD_IS_WEAK,
+    },
+  )
+  @Compare('password', {
+    message: UserServiceValidationError.PASSWORDS_CONFLICT,
+  })
   confirmPassword: string;
 }
