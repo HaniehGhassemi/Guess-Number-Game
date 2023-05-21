17c5d05e56b552fa813ec1398d42e32a0811e175
sajjad ghorbani <sajjadr2001@gmail.com>
2023-05-21 22:09:36 +0330

    Refactor: move signup and signin to auth service

--- back/src/modules/users/users.controller.ts
+++ back/src/modules/users/users.controller.ts
@@ -1,33 +1,11 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Query,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
-import { SignUpDto } from './dto/sign-up.dto';
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { SignInDto } from './dto/sign-in.dto';
-import { SignUpResponseDto } from './dto/sign-up-response.dto';
-import { SignInResponseDto } from './dto/sign-in-response.dto';
 import { GetUserInfo } from './dto/get-user-info-response.dto';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
-  @Post('signup')
-  signUp(@Body() signUpDto: SignUpDto): Promise<SignUpResponseDto> {
-    return this.userService.signUp(signUpDto);
-  }
-
-  @Post('signin')
-  signIn(@Body() signInDto: SignInDto): Promise<SignInResponseDto> {
-    return this.userService.signIn(signInDto);
-  }
-
   @Get('get-user-info')
   @UseGuards(AuthGuard())
   getUserInfo(@Req() req): Promise<GetUserInfo> {
