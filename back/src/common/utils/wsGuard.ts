e85b28602b569c1a1ecc38fd7b9237057783e233
Sajjad Ghorbani <sajjadr2001@gmail.com>
2023-06-05 23:45:37 +0330

    Refactor: user gateway - add guard

---
+++ back/src/common/utils/wsGuard.ts
@@ -0,0 +1,38 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Logger } from '@nestjs/common';
+import { Socket } from 'socket.io';
+@Injectable()
+export class WsGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const client: Socket = context.switchToWs().getClient<Socket>();
+    const token = this.extractTokenFromHeader(client);
+    Logger.log(token);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET_KEY,
+      });
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+      context.switchToHttp().getRequest().user = payload;
+    } catch (err) {
+      throw new UnauthorizedException(err);
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(client: Socket): string | undefined {
+    const token = client.handshake.headers?.authorization?.split(' ')[1];
+    return token;
+  }
+}
--- back/src/modules/users/user-gateway.ts
+++ back/src/modules/users/user-gateway.ts
@@ -1,13 +1,17 @@
-import { OnModuleInit } from '@nestjs/common';
+import { OnModuleInit, UseGuards } from '@nestjs/common';
 import {
+  ConnectedSocket,
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { Logger } from '@nestjs/common';
+import { Logger } from '@nestjs/common/services';
 import { UserGateWayConstants } from './types/user-gateway.enum';
+import { WsGuard } from 'src/common/utils/wsGuard';
 @WebSocketGateway({
   cors: true,
 })
@@ -21,20 +25,26 @@ export class UserGateWay
   }
 
   async handleConnection(client: Socket) {
-    //room here will be the userId
-    //each user is added to a room
-    client.on(UserGateWayConstants.JOIN_ROOM_ON, function (room: string) {
-      client.join(room);
-      Logger.log(`${client.id} connected to room: ${room}`);
-    });
+    Logger.log(`client ${client.id} connected`);
   }
   handleDisconnect(client: Socket) {
     Logger.log(`${client.id} disconnected`);
   }
 
+  @UseGuards(WsGuard)
+  @SubscribeMessage('join')
+  async handleEvent(
+    @MessageBody() room: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    //room here will be the userId
+    //each user is added to a room
+    client.join(room.toString());
+  }
+
   emitUserInfo(userId: string, userInfo: string) {
     this.server
-      .to(userId)
+      .to(userId.toString())
       .emit(UserGateWayConstants.GET_USER_INFO_EVENT, userInfo);
   }
 }
