6f56b0c1316e45ef3062eb5078485c87ccdd6b79
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-06-07 15:06:34 +0330

    Add: Sidebar

--- front/src/components/sidebar/Sidebar.vue
+++ front/src/components/sidebar/Sidebar.vue
@@ -1,36 +1,69 @@
 <template>
-  <div class="sidebar-container">
-    <div class="sidebar-section">
-      <span class="selection">
-        <a href="#" class="navigation"
-          ><fa class="icons" icon="fa-solid fa-house" />Home</a
-        ></span
-      >
-      <span class="selection"
-        ><a href="#" class="navigation"
-          ><fa class="icons" icon="fa-solid fa-medal" />Top Player</a
-        ></span
-      >
-      <span class="selection">
-        <a href="#" class="navigation"
-          ><fa class="icons" icon="fa-solid fa-cart-shopping" />Cart</a
-        ></span
-      >
-      <div class="setting-section">
-        <span class="selection">
-          <a href="#" class="navigation"
-            ><fa icon="fa-solid fa-gear" />Setting</a
-          ></span
-        >
-      </div>
+  <div class="sidebar">
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0"
+    />
+    <div class="menu">
+      <ul class="list-style">
+        <li>
+          <a href="/">
+            <span class="material-symbols-rounded"> home </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="material-symbols-rounded"> stadia_controller </span>
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="redirectUser">
+            <span class="material-symbols-rounded"> workspace_premium </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="material-symbols-rounded"> account_tree </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="material-symbols-rounded"> settings </span>
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent, ref, onMounted } from "vue";
+import { getUserInfo } from "@/services/getUserInfo";
+
+export default defineComponent({
   name: "Sidebar-vue",
-};
+  setup() {
+    const user = ref();
+    async function redirectUser() {
+      let routes = [];
+      if (user.value != null) {
+        routes = [{ path: "/guess-number" }];
+      }
+      routes = [{ path: "/user-register" }];
+    }
+
+    onMounted(async () => {
+      const {
+        data: { data: userInfo },
+      } = await getUserInfo();
+      user.value = userInfo;
+    });
+    return {
+      user,
+      redirectUser,
+    };
+  },
+});
 </script>
 
 <style lang="scss" src="./Sidebar.scss"></style>
