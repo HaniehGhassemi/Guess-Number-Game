351382285b3736132f79133429f231f70ee85e99
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-06-07 15:05:38 +0330

    Add: SlideShow

---
+++ front/src/components/slideShow/SlideShow.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="slideshow-section">
+    <Carousel
+      :navigation="false"
+      :pagination="true"
+      :startAutoPlay="true"
+      v-slot="{ currentSlide }"
+    >
+      <Slide v-for="(slide, index) in carouselSlides" :key="index">
+        <div v-show="currentSlide === index + 1" class="slide-info">
+          <img :src="require(`../../assets/images/slider/${slide}.jpg`)" />
+        </div>
+      </Slide>
+    </Carousel>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Carousel from "@/components/slideShow/Carousel.vue";
+import Slide from "@/components/slideShow/Slide.vue";
+export default defineComponent({
+  components: {
+    Carousel,
+    Slide,
+  },
+  setup() {
+    const carouselSlides = ["bg-1", "bg-2"];
+    return { carouselSlides };
+  },
+});
+</script>
+
+<style lang="scss" src="./SlideShow.scss"></style>
