68c2db49bdd03bea95bf6d11b0f4a74da89bf96f
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-06-08 08:57:59 +0330

    Refactor: add validation and change ui

--- front/src/components/userRegister/signIn/SignIn.vue
+++ front/src/components/userRegister/signIn/SignIn.vue
@@ -1,25 +1,30 @@
 <template>
   <AppContainer>
     <Container>
-      <div class="form-section">
-        <h1>Sign IN</h1>
-        <input
-          v-model="signInData"
-          name="signData"
-          type="text"
-          placeholder="Username or Email"
-        />
-        <input
-          v-model="password"
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-        />
+      <div class="signin-section">
+        <div class="signin-title">
+          <h1>Sign in</h1>
+        </div>
+        <div class="signin-form">
+          <input
+            v-model="usernameOrEmail"
+            name="signData"
+            type="text"
+            placeholder="Username or Email"
+          />
+          <input
+            v-model="password"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <p class="signin-error">{{ errorMessage }}</p>
+        </div>
+        <Button @click="signIn">Sign in</Button>
         <a href="/forget-pass"
           ><span data-id="resetpass-section">Forgot Password?</span></a
         >
-        <Button @click="signIn">Sign in</Button>
       </div>
     </Container>
   </AppContainer>
@@ -27,4 +32,4 @@
 
 <script lang="ts" src="./signIn"></script>
 
-<style lang="scss" src="../UserRegisterStyles.scss"></style>
+<style lang="scss" src="./Signin.scss"></style>
--- front/src/components/userRegister/signIn/signIn.ts
+++ front/src/components/userRegister/signIn/signIn.ts
@@ -1,8 +1,10 @@
 import AppContainer from "@/components/base/AppContainer.vue";
 import Container from "@/components/container/Container.vue";
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import axios from "axios";
 import Button from "@/components/Buttons/Button.vue";
+import router from "@/router";
+import { gerErrorMessage } from "@/services/ErrorHandling";
 
 export default defineComponent({
   name: "SignIn",
@@ -11,28 +13,42 @@ export default defineComponent({
     Container,
     Button,
   },
-  data() {
-    return {
-      signInData: "",
-      password: "",
-    };
-  },
-  methods: {
-    async signIn() {
-      const signInData = this.signInData;
-      const password = this.password;
-      const result = await axios.post(
-        `${process.env.VUE_APP_BASE_API_URL}/auth/signin`,
-        {
-          usernameOrEmail: signInData,
-          password: password,
+  setup() {
+    const usernameOrEmail = ref();
+    const password = ref();
+    const errorMessage = ref("");
+    async function signIn() {
+      try {
+        const result = await axios.post(
+          `${process.env.VUE_APP_BASE_API_URL}/auth/signin`,
+          {
+            usernameOrEmail: usernameOrEmail.value,
+            password: password.value,
+          }
+        );
+        if (result.status === 201) {
+          localStorage.setItem("token", result.data.data.token);
+          router.push({ name: "Home" });
         }
-      );
-      console.log(result);
-      if (result.status == 201) {
-        localStorage.setItem("token", result.data.data.token);
-        this.$router.push({ name: "Home" });
+      } catch (error: any) {
+        const message: string = Array.isArray(error.response.data.data.message)
+          ? error.response.data.data.message[0]
+          : error.response.data.data.message;
+        const errr: string = gerErrorMessage(message);
+        errorMessage.value = errr;
       }
-    },
+    }
+    onMounted(async () => {
+      if (localStorage.getItem("token")) {
+        router.push({ name: "Home" });
+      }
+    });
+
+    return {
+      usernameOrEmail,
+      password,
+      signIn,
+      errorMessage,
+    };
   },
 });
