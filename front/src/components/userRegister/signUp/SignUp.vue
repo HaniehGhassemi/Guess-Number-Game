f57b45a06f8d11aff5078686637f21ce2948c66a
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-06-08 08:57:15 +0330

    Refactor: add vlaidation and change ui

--- front/src/components/userRegister/signUp/SignUp.vue
+++ front/src/components/userRegister/signUp/SignUp.vue
@@ -1,49 +1,54 @@
 <template>
   <AppContainer>
-    <container>
-      <div class="form-section">
-        <h1>Sign Up</h1>
-        <input
-          v-model="fullname"
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Full Name"
-        />
-        <input
-          v-model="username"
-          type="text"
-          name="username"
-          id="username"
-          placeholder="Username"
-        />
-        <input
-          v-model="email"
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-        />
-        <input
-          v-model="password"
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-        />
-        <input
-          v-model="confirmPassword"
-          type="password"
-          name="re-password"
-          id="re-password"
-          placeholder="Repeat Password"
-        />
-        <Button @click="signUp">Sign Up</Button>
+    <Container>
+      <div class="signUp-section">
+        <div class="signUp-title">
+          <h2>Sign Up</h2>
+        </div>
+        <div class="signUp-form">
+          <input
+            v-model="fullname"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Full Name"
+          />
+          <input
+            v-model="username"
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+          />
+          <input
+            v-model="email"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
+          <input
+            v-model="password"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <input
+            v-model="confirmPassword"
+            type="password"
+            name="re-password"
+            id="re-password"
+            placeholder="Repeat Password"
+          />
+          <p class="signUp-error">{{ errorMessage }}</p>
+          <Button @click="signUp">Sign Up</Button>
+        </div>
       </div>
-    </container>
+    </Container>
   </AppContainer>
 </template>
 
 <script lang="ts" src="./signup.ts"></script>
 
-<style lang="scss" src="../UserRegisterStyles.scss"></style>
+<style lang="scss" src="./SignUp.scss"></style>
--- front/src/components/userRegister/signUp/signup.ts
+++ front/src/components/userRegister/signUp/signup.ts
@@ -1,35 +1,57 @@
 import AppContainer from "@/components/base/AppContainer.vue";
 import Container from "@/components/container/Container.vue";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import axios from "axios";
 import Button from "@/components/Buttons/Button.vue";
+import { gerErrorMessage } from "@/services/ErrorHandling";
+import { useRouter } from "vue-router";
+
 export default defineComponent({
   components: {
     AppContainer,
     Container,
     Button,
   },
-  methods: {
-    async signUp() {
-      const fullname = "";
-      const username = "";
-      const email = "";
-      const password = "";
-      const confirmPassword = "";
-      const result = await axios.post(
-        `${process.env.VUE_APP_BASE_API_URL}/auth/signup`,
-        {
-          fullname,
-          username,
-          email,
-          password,
-          confirmPassword,
+  setup() {
+    const router = useRouter();
+    const fullname = ref();
+    const username = ref();
+    const email = ref();
+    const errorMessage = ref("");
+    const password = ref();
+    const confirmPassword = ref();
+    async function signUp() {
+      try {
+        const result = await axios.post(
+          `${process.env.VUE_APP_BASE_API_URL}/auth/signup`,
+          {
+            fullname: fullname.value,
+            username: username.value,
+            email: email.value,
+            password: password.value,
+            confirmPassword: confirmPassword.value,
+          }
+        );
+        if (result.status == 201) {
+          router.push({ name: "SignIn" });
         }
-      );
-      if (result.status == 201) {
-        this.$router.push({ name: "SignIn" });
+      } catch (error: any) {
+        const message: string = Array.isArray(error.response.data.data.message)
+          ? error.response.data.data.message[0]
+          : error.response.data.data.message;
+        const errr: string = gerErrorMessage(message);
+        errorMessage.value = errr;
       }
-      console.log(result);
-    },
+    }
+
+    return {
+      signUp,
+      errorMessage,
+      fullname,
+      username,
+      email,
+      password,
+      confirmPassword,
+    };
   },
 });
