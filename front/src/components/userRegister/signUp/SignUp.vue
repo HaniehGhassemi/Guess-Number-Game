68f46ea9d5e531d864ac9876ef48be35f71c1bda
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-06-09 16:15:04 +0330

    Refactor:  new ui in sign up

--- front/src/components/userRegister/signUp/SignUp.vue
+++ front/src/components/userRegister/signUp/SignUp.vue
@@ -1,51 +1,49 @@
 <template>
   <AppContainer>
-    <Container>
-      <div class="signUp-section">
-        <div class="signUp-title">
-          <h2>Sign Up</h2>
-        </div>
-        <div class="signUp-form">
-          <input
-            v-model="fullname"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Full Name"
-          />
-          <input
-            v-model="username"
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Username"
-          />
-          <input
-            v-model="email"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-          />
-          <input
-            v-model="password"
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-          />
-          <input
-            v-model="confirmPassword"
-            type="password"
-            name="re-password"
-            id="re-password"
-            placeholder="Repeat Password"
-          />
-          <p class="signUp-error">{{ errorMessage }}</p>
-          <Button @click="signUp">Sign Up</Button>
-        </div>
+    <div class="signUp-section">
+      <div class="signUp-title">
+        <h2>Sign Up</h2>
       </div>
-    </Container>
+      <div class="signUp-form">
+        <input
+          v-model="fullname"
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Full Name"
+        />
+        <input
+          v-model="username"
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Username"
+        />
+        <input
+          v-model="email"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+        />
+        <input
+          v-model="password"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+        />
+        <input
+          v-model="confirmPassword"
+          type="password"
+          name="re-password"
+          id="re-password"
+          placeholder="Repeat Password"
+        />
+        <p class="signUp-error">{{ errorMessage }}</p>
+        <Button @click="signUp">Sign Up</Button>
+      </div>
+    </div>
   </AppContainer>
 </template>
 
